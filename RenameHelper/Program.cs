c4c5ac88aaa15a4bb3c256772249ce586e2ef324
Elias Jurado <i201611913@cibertec.edu.pe>
2023-02-14 04:10:08 -0500

    loading sources

---
+++ RenameHelper/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace RenameHelper
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            DirectoryRenamer.RenameDirectoryTree(
+            @"E:\My Drive",
+            name => name.Replace("_", "-"));
+        }
+    }
+}
